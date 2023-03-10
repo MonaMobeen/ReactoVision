da67a1cf0c959b0a2a7d61d33c23781d26aa0cc5
Memoona Mobeen <memoonamobeen618@gmail.com>
2023-03-10 19:27:46 +0000

    Oct2022SemV-ExploringReactt

--- 15-cocktails/setup/src/components/Cocktail.js
+++ 15-cocktails/setup/src/components/Cocktail.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Cocktail({ image, name, id, info, glass }) {
   return (
+    
     <article className='cocktail'>
       <div className='img-container'>
         <img src={image} alt={name} />
